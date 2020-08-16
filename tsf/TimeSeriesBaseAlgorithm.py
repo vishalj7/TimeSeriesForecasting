2becf65fc0023d0230a4d264bef9140deac2edaf
vishal_7 <vishal_7@hotmail.co.uk>
2020-08-16 16:48:57 +0100

    added comments to the base clas

--- tsf/TimeSeriesBaseAlgorithm.py
+++ tsf/TimeSeriesBaseAlgorithm.py
@@ -1,4 +1,6 @@
 
+
+# Libraries
 from abc import ABC
 from abc import abstractmethod
 
@@ -9,14 +11,22 @@ from statsmodels.tsa.stattools import adfuller
 
 
 class TimeSeriesBaseAlgorithm(ABC):
-    """Base class for all time series algorithms
+    """
+    Base class for all time series algorithms. Every algorithm
+    should inherit from this class and those become a subclass of 
+    TimeSeriesBaseAlgorithm. The subclass needs to implement the
+    abstract methods. Where required the subclass can overwrite 
+    any of the concrete methods here. 
 
     Notes
     -----
-    All estimators should specify all the parameters that can be set
-    at the class level in their ``__init__`` as explicit keyword
-    arguments (no ``*args`` or ``**kwargs``).
+    
     """
+    
+    @abstractmethod   
+    def cross_validate(self):
+        pass
+    
     
     @abstractmethod   
     def grid_search_run(self):
@@ -29,12 +39,7 @@ class TimeSeriesBaseAlgorithm(ABC):
 
 
     @abstractmethod   
-    def forecast_vs_actual_plot(self):
-        pass
-
-
-    @abstractmethod   
-    def cross_validate(self):
+    def plot_forecasts(self):
         pass
 
 
@@ -61,7 +66,7 @@ class TimeSeriesBaseAlgorithm(ABC):
             A dataframe which can be used for testing the model
         """
         
-        # Creating the train and validation set
+        # Creating the train and validation set by splitting the data
         train = data[:int(split_proportion*(len(data)))]
         test = data[int(split_proportion*(len(data))):]
 
@@ -90,10 +95,13 @@ class TimeSeriesBaseAlgorithm(ABC):
         
         timeseries = timeseries.iloc[:,0].values    
 
-        #Perform Dickey-Fuller test:
-        
+        # Performs Dickey-Fuller test
         df_test_res = adfuller(timeseries, autolag='AIC')
+
+        # Creates Series and specifies the index
         df_res_output = pd.Series(df_test_res[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+
+        # For each critical value is given the 'Critical Value' title plus the relevant percentage
         for key,value in df_test_res[4].items():
             df_res_output['Critical Value (%s)'%key] = value
 
@@ -119,15 +127,17 @@ class TimeSeriesBaseAlgorithm(ABC):
             The line plot for the rolling mean and standard deviation
 
         """
+
         print("Displaying rolling plot...")
-        timeseries = timeseries.iloc[:,0].values             # only selects the value data points
+        # Selects only the value data points and creates an empty plot
+        timeseries = timeseries.iloc[:,0].values             
         fig, ax = plt.subplots(figsize=(20, 10))
 
-        #Determing rolling statistics
+        # Calculates the rolling statistics for both the mean and standard deviation
         rolmean = pd.Series(timeseries).rolling(rolling_period).mean()
         rolstd = pd.Series(timeseries).rolling(rolling_period).std()
 
-        #Plot rolling statistics:
+        # Plots rolling statistics
         orig = plt.plot(timeseries, color='blue',label='Original')
         mean = plt.plot(rolmean, color='red', label='Rolling Mean')
         std = plt.plot(rolstd, color='black', label = 'Rolling Std')
@@ -159,9 +169,11 @@ class TimeSeriesBaseAlgorithm(ABC):
             The mean of the absolute percentage error between the actual and predicted value/s to 2 decimal places
         """
         
+        # Returns a contiguous flattened array
         y_true = np.array(y_true).ravel()
         y_pred = np.array(y_pred).ravel()
     
+        # Calculates the MAPE
         mape = round(np.mean(np.abs((y_true - y_pred) / y_true)) * 100, 2)
 
         return mape
